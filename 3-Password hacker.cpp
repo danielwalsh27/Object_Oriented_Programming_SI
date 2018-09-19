9b852eb37238502c7018bca9b150bb990926118c
Daniel Walsh <dewalsh27@yahoo.com>
2018-09-18 22:04:28 -0700

    Update 3-Password hacker.cpp

--- 3-Password hacker.cpp
+++ 3-Password hacker.cpp
@@ -27,30 +27,30 @@ int main(){
 				for(int o= 0;o < 10; o++){ 	//10^1 = 10
 				for(int p= 0;p < 10; p++){ 	//10^0 = 1
 
-					//we had to multiple each number by its respective power, use this graph ^^^
-					int newI = i * 10000000; int newJ = j * 1000000;
-					int newK = k * 100000; int newL = l * 10000;
-					int newM = m * 1000; int newN = n * 100;
-					int newO = o * 10; int newP = p * 1;
+				//we had to multiple each number by its respective power, use this graph ^^^
+				int newI = i * 10000000; int newJ = j * 1000000;
+				int newK = k * 100000; int newL = l * 10000;
+				int newM = m * 1000; int newN = n * 100;
+				int newO = o * 10; int newP = p * 1;
 
-					//adding all these together gives us our new number that we need to compare to the password
-					temp = newI + newJ + newK + newL +newM + newN + newO + newP;
-					if(temp == pass){
-						cout << "\nWe were able to hack your password, it took us " << temp + 1 << " attempts.\n" << endl;
-						cout << "Also, we did 100,000,000 (one hundred million) iterations on this program." << endl;
-						cout << "Each for loop looks as such:" << endl;
-						cout << "\t Loop P completes its 10 iterations first (10^1 = 10)" << endl;
-						cout << "\t Loop O then completes its 10 iterations, but that means that Loop P has completed a total of 100 iterations (10^2 = 100)"<< endl;
-						cout << "\t Loop N does its 10 iterations (10^3 = 1,000 total iterations for Loop P now)" << endl;
-						cout << "\t Loop M does its 10 iterations (10^4 = 10,000 total iterations for Loop P now)" << endl;
-						cout << "\t Loop L does its 10 iterations (10^5 = 100,000 total iterations for Loop P now)" << endl;
-						cout << "\t Loop K does its 10 iterations (10^6 = 1,000,000 total iterations for Loop P now)" << endl;
-						cout << "\t Loop J does its 10 iterations (10^7 = 10,000,000 total iterations for Loop P now)" << endl;
-						cout << "\t Loop I does its 10 iterations (10^8 = 100,000,000 total iterations for Loop P now)" << endl;
-						trigger = true;
-					}
-//					else{
-//						cout << "Couldn't hack password. You win! ";
+				//adding all these together gives us our new number that we need to compare to the password
+				temp = newI + newJ + newK + newL +newM + newN + newO + newP;
+				if(temp == pass){
+					cout << "\nWe were able to hack your password, it took us " << temp + 1 << " attempts.\n" << endl;
+					cout << "Also, we did 100,000,000 (one hundred million) iterations on this program." << endl;
+					cout << "Each for loop looks as such:" << endl;
+					cout << "\t Loop P completes its 10 iterations first (10^1 = 10)" << endl;
+					cout << "\t Loop O then completes its 10 iterations, but that means that Loop P has completed a total of 100 iterations (10^2 = 100)"<< endl;
+					cout << "\t Loop N does its 10 iterations (10^3 = 1,000 total iterations for Loop P now)" << endl;
+					cout << "\t Loop M does its 10 iterations (10^4 = 10,000 total iterations for Loop P now)" << endl;
+					cout << "\t Loop L does its 10 iterations (10^5 = 100,000 total iterations for Loop P now)" << endl;
+					cout << "\t Loop K does its 10 iterations (10^6 = 1,000,000 total iterations for Loop P now)" << endl;
+					cout << "\t Loop J does its 10 iterations (10^7 = 10,000,000 total iterations for Loop P now)" << endl;
+					cout << "\t Loop I does its 10 iterations (10^8 = 100,000,000 total iterations for Loop P now)" << endl;
+					trigger = true;
+				}
+//				else{
+//					cout << "Couldn't hack password. You win! ";
 //					}
 				}
 				}
