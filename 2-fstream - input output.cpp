da357fa045877d1beb76f16ab08a72134c815f40
Daniel Walsh <dewalsh27@yahoo.com>
2018-09-18 22:12:10 -0700

    Update 2-fstream - input output.cpp

--- 2-fstream - input output.cpp
+++ 2-fstream - input output.cpp
@@ -1,10 +1,10 @@
 //create an array to hold 5 sentences
 //open the file titled "holds5lines.txt"
-//load the array with the 5 sentences from the txt file
+//load the array with the 5 sentences from the .txt file
 //Print all 5 sentences
 //create a file titled "createThisFile.txt"
 //reverse load the array into the new file
-//since we had extra time we also added an if/else statment to only have 5 total lines in our output file
+//since we had extra time we also added an if/else statement to only have 5 total lines in our output file
 
 #include <iostream>
 #include <fstream>
@@ -39,15 +39,15 @@ int main(){
 			outputFile << strArr[i] << endl;
 		else
 			outputFile << strArr[i];
-		//outputFile << strArr[i] << endl;
+		//outputFile << strArr[i] << endl; //this works but it'll create an extra line
 	}
 
 	//outputFile << strArr[4] << "\n" << strArr[3] << "\n" << strArr[2] << "\n" << strArr[1] << "\n" << strArr[0];
 	outputFile.close();
 	return 0;
 }
-//REQUIRED LESSON- We spent 20 mintues teaching how fstream works, more specifally the use of ofstream and ifstream.
-//We covered how it has preloaded fucntions like .open() and .close(), and the proper use of them. We also when over that
+//REQUIRED LESSON- We spent 20 minutes teaching how fstream works, more specifically the use of ofstream and ifstream.
+//We covered how it has pre-loaded functions like .open() and .close(), and the proper use of them. We also when over that
 // ofstream can create a file by the use of the .open() function.
 
 //We must use the attached file "2-holds5lines.txt"
