4a824f2e718d9d6110a0bf9c97b8478d2dfaf45d
Daniel Walsh <dewalsh27@yahoo.com>
2018-12-19 12:44:37 -0800

    Update 4.2-Student Class.cpp

--- 4.2-Student Class.cpp
+++ 4.2-Student Class.cpp
@@ -1,43 +1,51 @@
-  #include <iostream>
-//#include <string>
+//This program creates a student object and has a class object within it, there are
+//two constructors and 5 member functions that are defined outside of the class.
+//It also used a LinkedList to store classes taken, this is edited within the
+//loadClasses() function
+
+#include <iostream>
 #include <list>
 using namespace std;
 
+//create a class of type Student
 class Student{
-private:
+private:		//Private members - can only be accessed through member functions
 	string fName, lName;
 	string address, city, state;
 	int zip;
 	long CWID, phoneNum;
 	string birthDate, email, major;
-	double GPA;
-
-	struct Classes{
+	double gpa;
+//creating a struct of Classes
+	struct Classes{					//public by default
 		string semYear, course;
 		char grade;
 		double gradePt;
 		int crHour;
 	};
-
+//use linked listed to store different classes used
 	list<Classes> myClass;
-public:
-	Student();
-	Student(string fName, string lName, int CWID, string email, string major);
+
+public:		//Public members - can be accessed from main
+	Student();							//default constructor
+	Student(string fName, string lName, int CWID, string email, string major);	//parameterized constructor
 	void loadPartial();
 	void printData();
 	void loadClasses();
 	void printClasses();
-	double calcGPA();
+	double calcgpa();
 };
-
+//default constructor- setting all values to NULL
 Student::Student(){
 	fName = "", lName = "", address = "", city = " ", state = "", zip = 99999, CWID = 999999999, phoneNum = 9999999999,
-			birthDate = "", email = "", major = "", GPA = 0.0;
+			birthDate = "", email = "", major = "", gpa = 0.0;
 }
+//parameterized constructor- setting the values to what was passed in
 Student::Student(string first, string last, int cwid, string eMail, string maj){
 	fName = first; lName = last; CWID = cwid; email = eMail; major = maj;
-	address = "", city = " ", state = "", zip = 99999, phoneNum = 9999999999, birthDate = "", GPA = 0.0;
+	address = "", city = " ", state = "", zip = 99999, phoneNum = 9999999999, birthDate = "", gpa = 0.0;
 }
+//student function used to load all user data
 void Student::loadPartial(){
 	cout << "Hi " << fName << ".\nPlease enter your house number and address: "; getline(cin, address);
 	cout << "Enter your city:"; getline(cin,city);
@@ -46,6 +54,7 @@ void Student::loadPartial(){
 	cout << "Enter your birthdate (MM/DD/YYYY):"; getline(cin, birthDate);
 	cout << "Enter your phone number:"; cin >> phoneNum; cin.ignore();
 }
+//student function used to print all user data
 void Student::printData(){
 	cout << "Name: " << "\t\t"<< fName << " " << lName;
 	cout << "\n\tAddress: " << "\t"<< address << ", " << city << ", " << state << ". "<< zip;
@@ -54,13 +63,14 @@ void Student::printData(){
 	cout << "\n\tMajor: " << "\t\t"<< major;
 	cout << "\n\tPhone Number: " << "\t"<< phoneNum;
 	cout << "\n\tDOB: " << "\t\t"<< birthDate;
-	cout << "\n\tGPA: " << "\t\t" << calcGPA() << endl;
+	cout << "\n\tgpa: " << "\t\t" << calcgpa() << endl;
 }
+//student function used to load classes for that student
 void Student::loadClasses(){
 	cout << "\n\tYou are loading classes into your list" << endl;
 	char cont = ' ';
-	Classes temp;
-	do{
+	Classes temp;				//create a temporary object
+	do{							//load temporary object
 		cout << "Enter the Course and Number (CPSC 120): "; getline(cin,temp.course);
 		cout << "Enter the Semester and Year (Spring 2017): "; getline(cin, temp.semYear);
 		cout << "Enter the grade you received: "; cin >> temp.grade;
@@ -72,10 +82,11 @@ void Student::loadClasses(){
 		else if(temp.grade == 'F' || temp.grade == 'f') temp.gradePt = 0.0;
 		else cout << "\n\tInvalid Input";
 		temp.gradePt = temp.gradePt * temp.crHour;
-		myClass.push_front(temp);
+		myClass.push_front(temp);	//push temp object into data structure
 		cout << "Another entry?  (Y to continue, N to exit): "; cin >> cont; cin.ignore();
 	} while (cont == 'Y' || cont == 'y');
 }
+//student function used to print the classes for that student
 void Student::printClasses(){
 	cout << "**Classes taken at CSUF are: **" << endl;
 	for(list<Classes>::iterator it = myClass.begin(); it != myClass.end(); it ++){
@@ -84,21 +95,24 @@ void Student::printClasses(){
 		cout << "\tThe grade is: \t\t" << it->grade << endl;
 	}
 }
-double Student::calcGPA(){
-	double gpa=0;
-	int creditTotal=0;
+//student function used to print the gpa of the student
+double Student::calcgpa(){
+	double totalGradePts=0;
+	int totalCredits=0;
 	for(list<Classes>::iterator it = myClass.begin(); it != myClass.end(); it ++){
 	//for(auto i : myClass){
-		gpa = gpa + it->gradePt;
-		creditTotal = creditTotal + it->crHour;
+		totalGradePts = totalGradePts + it->gradePt;
+		totalCredits = totalCredits + it->crHour;
 	}
-	double ret = gpa / creditTotal;
-	return ret;
+	double gpa = totalGradePts / totalCredits;
+	return gpa;
 }
+
+//in main, create student object and call all functions in order.
 int main(){
 	Student Tuffy("Tuffy", "Titan", 888999333, "Tuffy@student.edu", "Computer Science");
 	Tuffy.loadPartial();
 	Tuffy.loadClasses();
-	Tuffy.printData();
+	Tuffy.printData();					//calls calcgpa
 	Tuffy.printClasses();
 }
